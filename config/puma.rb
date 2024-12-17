a87a74657bcb64c69c4bd6f4790a02bfcf991225
alexreginelli <108173894+alexreginelli@users.noreply.github.com>
2024-12-17 23:23:41 +0000

    fix puma and procfile

--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,19 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+
+##
+
+# Environment
+environment ENV.fetch("RAILS_ENV") { "production" }
+# Workers and threads
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+# Preload app for faster startup
+preload_app!
+
+# Allow puma to be restarted by `rails restart` command
+plugin :tmp_restart
