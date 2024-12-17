c979a4926f08892c08dcfd0eaf61b48a6f17c528
alexreginelli <108173894+alexreginelli@users.noreply.github.com>
2024-12-17 12:54:53 +0000

    final raw version

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,21 @@
 class ReportsController < ApplicationController
   def index
+    @stocktakes = Stocktake.all
     @products = Product.all
-    @previous_stock = StocktakeEntry.where(stocktake_id: params[:previous_id]).group(:product_id).sum(:quantity)
-    @current_stock = StocktakeEntry.where(stocktake_id: params[:current_id]).group(:product_id).sum(:quantity)
+  
+    @previous_stock = {}
+    @current_stock = {}
+  
+    if params[:stocktake_a_id] && params[:stocktake_b_id]
+      stocktake_a = Stocktake.find_by(id: params[:stocktake_a_id])
+      stocktake_b = Stocktake.find_by(id: params[:stocktake_b_id])
+  
+      if stocktake_a && stocktake_b
+        @previous_stock = stocktake_a.stocktake_entries.group(:product_id).sum(:quantity)
+        @current_stock = stocktake_b.stocktake_entries.group(:product_id).sum(:quantity)
+      else
+        flash[:alert] = "Stock take not found"
+      end
+    end
   end
 end
